fc9eff3bc7a3b4172c8671635a58b24d24a6e503
trevorelvey <trevor.elvey@gmail.com>
2016-05-10 15:05:09 -0700

    add user logins

---
+++ app/src/main/java/com/epicodus/guest/movietracker/Constants.java
@@ -0,0 +1,10 @@
+package com.epicodus.guest.movietracker;
+
+public class Constants {
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String KEY_USER_EMAIL = "email";
+}
