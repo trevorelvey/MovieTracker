f5c71261033390dfac89731a8695c18aea3834c6
trevorelvey <trevor.elvey@gmail.com>
2016-05-10 15:57:49 -0700

    add add movie activity

---
+++ app/src/main/java/com/epicodus/guest/movietracker/ui/AddMovie.java
@@ -0,0 +1,15 @@
+package com.epicodus.guest.movietracker.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.guest.movietracker.R;
+
+public class AddMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_movie);
+    }
+}
