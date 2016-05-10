f352bdffdf3d8030b0162e33c9631519db5a7811
trevorelvey <trevor.elvey@gmail.com>
2016-05-10 16:08:12 -0700

    set intent for button to add movie activity

--- app/src/main/java/com/epicodus/guest/movietracker/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/guest/movietracker/ui/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.guest.movietracker.Constants;
 import com.epicodus.guest.movietracker.R;
 import com.firebase.client.Firebase;
 
+import butterknife.Bind;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.addMovieButton) Button mAddMovieButton;
+    @Bind(R.id.movieListButton) Button mMovieListButton;
     private Firebase mFirebaseRef;
 
     @Override
@@ -20,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+
+        mAddMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddMovie.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
